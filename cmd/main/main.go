3ab8997be6eeac3712cf388afabd6ccd47c9452a
Jacob Lee <jacob.juhyung.lee@gmail.com>
2022-09-06 16:59:43 +0900

    Add main

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/jacobjlee/go-bookstore/pkg/routes"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
